9d50264e45d13b990812d6805891dd6aa77f5e45
nam334 <namratadas334@gmail.com>
2025-04-19 18:56:36 +0530

    Completed features of adding a file editing and deleting files/folders.

--- fileexplorer/fileexplorerproject/src/components/FileExplorerMenu.jsx
+++ fileexplorer/fileexplorerproject/src/components/FileExplorerMenu.jsx
@@ -1,35 +1,162 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const FileExplorerMenu = ({ dataSetOne }) => {
+const FileExplorerMenu = ({
+  dataSetOne,
+  setFileExplorerData,
+  deleteItemHandler,
+  addItem,
+  selectType,
+  setSelectType,
+  addValue,
+  editValue,
+  editSelectHandler,
+  setAddValue,
+  setEditValue,
+}) => {
   const [toggle, setToggle] = useState(false);
-  const { name, type } = dataSetOne;
-  const children = dataSetOne?.children;
+  const [showInput, setShowInput] = useState(false);
+
+  const [showEditInput, setEditInput] = useState(false);
+
+  const { name = "", type = "", id = "" } = dataSetOne || {};
+  console.log(dataSetOne?.children?.length, toggle);
   const toggleItems = () => setToggle((prev) => !prev);
+  const addItemHandler = () => {
+    setShowInput(true);
+  };
+  const closeItemHandler = () => {
+    setShowInput(false);
+  };
+  const cancelItemHandler = () => {
+    // setShowInput(false);
+    setAddValue("");
+    setSelectType("");
+  };
+
+  const editItemHandler = () => {
+    setEditInput((prev) => !prev);
+  };
+
+  const editCancelHandler = () => {
+    setEditValue("");
+  };
+
+  useEffect(() => {
+    console.log("toggle", toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    setEditValue(dataSetOne?.name);
+  }, [dataSetOne]);
   return (
     <>
       <div className="fileMenuParentItem">
         {type === "directory" ? "📁" : "📄"}
         {name}
-        {dataSetOne?.children?.length > 0 ? (
-          toggle ? (
-            <i
-              class="fa fa-angle-double-up"
-              aria-hidden="true"
-              onClick={toggleItems}
-            ></i>
-          ) : (
-            <i
-              class="fa fa-angle-double-down"
-              aria-hidden="true"
-              onClick={toggleItems}
-            ></i>
-          )
-        ) : null}
+        <span>
+          {dataSetOne?.children?.length > 0 ? (
+            toggle ? (
+              <i
+                className="fa fa-angle-double-up"
+                aria-hidden="true"
+                onClick={toggleItems}
+              ></i>
+            ) : (
+              <i
+                className="fa fa-angle-double-down"
+                aria-hidden="true"
+                onClick={toggleItems}
+              ></i>
+            )
+          ) : null}
+
+          {type === "directory" ? (
+            !showInput ? (
+              <button className="addButton" onClick={addItemHandler}>
+                +
+              </button>
+            ) : (
+              <button className="addButton" onClick={closeItemHandler}>
+                -
+              </button>
+            )
+          ) : null}
+
+          <button className="addButton" onClick={() => deleteItemHandler(id)}>
+            🗑️
+          </button>
+          <button className="addButton" onClick={editItemHandler}>
+            ✏️
+          </button>
+        </span>
       </div>
+      {showInput && (
+        <>
+          <div className="addItemContainer">
+            <input
+              type="text"
+              className="inputBox"
+              placeholder="Enter item name"
+              value={addValue}
+              onChange={(e) => setAddValue(e.target.value)}
+            />
+            <span>Select Type :</span>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                value="directory"
+                onChange={() => setSelectType("directory")}
+              />
+              Directory
+              <input
+                type="radio"
+                name="type"
+                value="file"
+                onChange={() => setSelectType("file")}
+              />
+              File
+            </div>
+            <div className="buttonContainer">
+              <button onClick={addItem}>Add</button>
+              <button onClick={cancelItemHandler}>Clear</button>
+            </div>
+          </div>
+        </>
+      )}
+      {showEditInput && (
+        <>
+          <div className="editItemContainer">
+            <input
+              type="text"
+              className="inputBox"
+              placeholder="Enter item name"
+              value={editValue}
+              onChange={(e) => setEditValue(e.target.value)}
+            />
+            <div>
+              <button onClick={() => editSelectHandler(id, name)}>✅</button>
+              <button onClick={editCancelHandler}>❌</button>
+            </div>
+          </div>
+        </>
+      )}
       {toggle > 0 ? (
         <>
-          {children?.map((childItem) => (
-            <FileExplorerMenu dataSetOne={childItem} />
+          {dataSetOne?.children?.map((childItem) => (
+            <FileExplorerMenu
+              key={childItem.id}
+              dataSetOne={childItem}
+              setFileExplorerData={setFileExplorerData}
+              deleteItemHandler={deleteItemHandler}
+              addItem={addItem}
+              selectType={selectType}
+              setSelectType={setSelectType}
+              addValue={addValue}
+              setAddValue={setAddValue}
+              editSelectHandler={(id, name) => editSelectHandler(id, name)}
+              setEditValue={setEditValue}
+            />
           ))}
         </>
       ) : null}
--- fileexplorer/fileexplorerproject/src/components/Home.jsx
+++ fileexplorer/fileexplorerproject/src/components/Home.jsx
@@ -1,12 +1,104 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FileExplorerMenu from "./FileExplorerMenu";
 import { dataSetOne } from "../utils/data";
+import { v4 as uuidv4 } from "uuid";
+
 const Home = () => {
+  const [fileExplorerData, setFileExplorerData] = useState(dataSetOne);
+  const [selectType, setSelectType] = useState("");
+  const [addValue, setAddValue] = useState("");
+  const [editValue, setEditValue] = useState(dataSetOne?.name);
+  useEffect(() => {
+    console.log("Original data is", fileExplorerData);
+  }, [fileExplorerData]);
+
+  const addItem = () => {
+    let parentName = dataSetOne?.name,
+      itemname;
+    if (selectType === "file") itemname = addValue + ".txt";
+    const item = {
+      id: uuidv4(),
+      name: selectType === "file" ? itemname : addValue,
+      type: selectType,
+    };
+    if (selectType === "directory") item.children = [];
+    //make a copy
+    const copyData = JSON.parse(JSON.stringify(fileExplorerData));
+    addToParent(copyData);
+    function addToParent(node) {
+      if (node.name === parentName) {
+        if (!node.children) node.children = [];
+        node.children.push(item);
+        return true;
+      }
+      if (node.children) {
+        for (let child of node.children) {
+          if (child.type === "directory") {
+            if (addToParent(child)) return true;
+          }
+        }
+      }
+    }
+
+    setFileExplorerData(copyData);
+  };
+  const deleteItemHandler = (id) => {
+    //check for root
+    if (dataSetOne?.id === id) {
+      setFileExplorerData(null);
+      return;
+    }
+
+    const removeData = (node) => {
+      if (!node.children) return;
+
+      node.children = node?.children?.filter((child) => child.id !== id);
+      for (const child of node.children) {
+        removeData(child);
+      }
+    };
+    const copyData = JSON.parse(JSON.stringify(fileExplorerData));
+    removeData(copyData);
+    setFileExplorerData(copyData);
+  };
+  const editSelectHandler = (id, name) => {
+    console.log("Parent is", id, name, "value is", editValue);
+
+    //COPY THE DATA
+    const copyData = JSON.parse(JSON.stringify(fileExplorerData));
+
+    const editName = (node) => {
+      if (node.id === id) {
+        node.name = editValue;
+        return true;
+      }
+      if (node.children) {
+        for (let child of node.children) {
+          if (editName(child)) return true;
+        }
+      }
+    };
+    editName(copyData);
+    setFileExplorerData(copyData);
+  };
   return (
     <>
+      <div className="explorer">EXPLORER</div>
       <div className="fileMenu">
         {" "}
-        <FileExplorerMenu dataSetOne={dataSetOne} />
+        <FileExplorerMenu
+          dataSetOne={fileExplorerData}
+          setFileExplorerData={setFileExplorerData}
+          deleteItemHandler={(id) => deleteItemHandler(id)}
+          addItem={addItem}
+          selectType={selectType}
+          setSelectType={setSelectType}
+          addValue={addValue}
+          setAddValue={setAddValue}
+          editValue={editValue}
+          editSelectHandler={(id, name) => editSelectHandler(id, name)}
+          setEditValue={setEditValue}
+        />
       </div>
     </>
   );
